faadc5d080fce96793e7d787698ba73d5ecfa818
Kashif <ahmed.kashif1001@gmail.com>
2023-08-16 22:19:11 +0500

    changes-1

---
+++ JavaScript Mastery/arrays.js
@@ -0,0 +1,11 @@
+// const myArray = ["value1", "value2"]
+
+// let [myvar1,myvar2] = myArray
+
+// console.log(myArray);
+
+let fruits = ["mango", "apple", "orange"]
+
+let numbers = [1,2,3,4,5]
+console.log(fruits[1]);
+console.log(numbers[0]);

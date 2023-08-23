0b6ac9b74df3f56eca3dbb951c97ce46ea07cb31
Kashif <ahmed.kashif1001@gmail.com>
2023-08-23 16:28:35 +0500

    arrays

---
+++ JavaScript Mastery/3-Loops/index.js
@@ -0,0 +1,30 @@
+//for loop
+
+// for(let i=0; i<10; i++){
+//     console.log("Hi");
+// }
+
+let i = 0;
+
+console.log(i++);
+console.log(i);
+console.log(i++);
+console.log(i);
+
+let j = 0;
+
+console.log(++j);
+console.log(j);
+console.log(++j);
+console.log(j);
+
+
+//while loop
+
+const k = 0;
+
+while(i < 10){
+
+console.log("Hi");
+
+}
---
+++ JavaScript Mastery/5-Arrays/index.js
@@ -0,0 +1,38 @@
+// const myArray = ["value1", "value2"]
+
+// let [myvar1,myvar2] = myArray
+
+// console.log(myArray);
+
+let fruits = ["mango", "apple", "orange"]
+
+let numbers = [1,2,3,4,5]
+console.log(fruits[1]);
+console.log(numbers[0]);
+
+
+let arr1 =[1,2,3,4]
+let arr2 =["kashif","ahmed"]
+
+console.log(arr1[3]);
+console.log(arr2[1]);
+
+
+//assigning values to an array using index
+let arr = [44,54];
+
+//we can change the values of our array by assigning new values to it later by using its index number, the value will be changed at the respective index number
+arr[0] = 10
+arr[1] = 11
+arr[2] = 12
+arr[3] = 13
+
+console.log(arr);
+
+//another way of initializing an array
+
+let arrr = new Array(1,2,3,4)
+
+arrr[0] = 12
+
+console.log(arrr);

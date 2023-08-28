147c04ac743ac38f15a79ee4c2ab17df5f6a0e1d
Kashif <ahmed.kashif1001@gmail.com>
2023-08-28 21:44:23 +0500

    DOM working

--- JavaScript Mastery/7-DOM/index.js
+++ JavaScript Mastery/7-DOM/index.js
@@ -1,2 +1,13 @@
-const thisOne = document.getElementById('okay')
-console.log(thisOne);
+// const thisOne = document.getElementById('okay')
+// console.log(thisOne);
+
+
+function hello(){
+    const heading = document.querySelector('h1');
+    if(heading.innerHTML === "Hello"){
+        heading.innerHTML = "Goodbye"
+    }
+    else{
+        heading.innerHTML = "Hello";
+    }
+}

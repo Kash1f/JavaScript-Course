caf482a55d693575cb83ef1ce9fc8e66dedf48db
Kashif <ahmed.kashif1001@gmail.com>
2023-09-05 22:14:33 +0500

    className

--- JavaScript Mastery/8-EventListeners/index.js
+++ JavaScript Mastery/8-EventListeners/index.js
@@ -24,6 +24,7 @@ document.getElementById("heading").addEventListener("click", function (e) {
     let variable;
     console.log("You have clicked the heading");
     variable = e.target;
+    variable = e.target.className;
     console.log(variable);
 });
 

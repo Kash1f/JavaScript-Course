36259233048733654428f6c4e22ca9c29d58ae7b
Kashif <ahmed.kashif1001@gmail.com>
2023-09-05 22:44:47 +0500

    offsetXY

--- JavaScript Mastery/8-EventListeners/index.js
+++ JavaScript Mastery/8-EventListeners/index.js
@@ -25,6 +25,15 @@ document.getElementById("heading").addEventListener("click", function (e) {
     console.log("You have clicked the heading");
     variable = e.target;
     variable = e.target.className;
+    variable = Array.from(e.target.classList);
+
+    variable = e.target.id;
+    variable = e.offsetX;
+    // variable = e.clientX;
+    // variable = e.clientY;
+
+
     console.log(variable);
+
 });
 

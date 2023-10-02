1c40947f3c38bb755f0b5f73a0e4a7d67b356a06
Kashif <ahmed.kashif1001@gmail.com>
2023-10-02 23:23:26 +0500

    foreach-loop

--- JavaScript Mastery/3-Loops/index.js
+++ JavaScript Mastery/3-Loops/index.js
@@ -4,27 +4,35 @@
 //     console.log("Hi");
 // }
 
-let i = 0;
+// let i = 0;
 
-console.log(i++);
-console.log(i);
-console.log(i++);
-console.log(i);
+// console.log(i++);
+// console.log(i);
+// console.log(i++);
+// console.log(i);
 
-let j = 0;
+// let j = 0;
 
-console.log(++j);
-console.log(j);
-console.log(++j);
-console.log(j);
+// console.log(++j);
+// console.log(j);
+// console.log(++j);
+// console.log(j);
 
 
-//while loop
+// //while loop
 
-const k = 0;
+// const k = 0;
 
-while(i < 10){
+// while(i < 10){
 
-console.log("Hi");
+// console.log("Hi");
 
+// }
+
+//for of loop
+
+const fruits = ["mango", "apple", "grapes"]
+
+for(let fruit of fruits){
+    console.log(fruit);
 }

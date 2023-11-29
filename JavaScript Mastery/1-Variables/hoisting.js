51b1ffbc6daf8c66f1846f73d8ba6c79be187980
Kashif <ahmed.kashif1001@gmail.com>
2023-11-29 22:17:45 +0500

    hoisting-example

--- JavaScript Mastery/1-Variables/hoisting.js
+++ JavaScript Mastery/1-Variables/hoisting.js
@@ -2,8 +2,11 @@
 //hoisting - variable and functions are hoisted, which means there declaration is moved on top of the code
 
 var a;
+var b;
 
 
 
 console.log(a);
+console.log(b);
 a = 100;
+b = 90;

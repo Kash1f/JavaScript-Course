e96fbc69d56017ebb8e9d39acc2d9336cee1cdae
Kashif <ahmed.kashif1001@gmail.com>
2023-11-29 22:17:01 +0500

    hoisting

---
+++ JavaScript Mastery/1-Variables/hoisting.js
@@ -0,0 +1,9 @@
+
+//hoisting - variable and functions are hoisted, which means there declaration is moved on top of the code
+
+var a;
+
+
+
+console.log(a);
+a = 100;
---
+++ JavaScript Mastery/7-Arrays/morearraymethods.js
@@ -0,0 +1,22 @@
+
+//forEach() method
+
+let abc = [45,55,65,99]
+
+abc.forEach(n => {
+    console.log(n);
+});
+
+//map fucntion
+
+const numbers = [3,4,6,1,8]  //array
+
+const square = function(number){   
+     //callback function to square the number, 3 and other numbers will pe passed one by one in this function which will be squared here
+    return number * number
+}
+
+const squareNumbers = numbers.map(square);   
+console.log(squareNumbers);
+
+  //map function that will give new array as an output. square variable will be passed in map function since it squares the number and the map function will give the required output when the variable in which map is stored will be printed 

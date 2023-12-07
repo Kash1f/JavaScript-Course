ac9e859a4ebbf5f3cb51ed9dd3626aeeef51e393
Kashif <ahmed.kashif1001@gmail.com>
2023-12-07 22:30:57 +0500

    completeTrue

--- JavaScript Mastery/ES6/Promises/promises.js
+++ JavaScript Mastery/ES6/Promises/promises.js
@@ -1,1 +1,4 @@
 //Promises are like real promises that we make. In Javascript promises are used to run a part of code. If the code executes then the promise is fulfilled otherwise the promise is rejected.
+
+
+let complete = true;

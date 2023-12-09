7f00c781cc65dd2a52fc10aae82f482bf8abc398
Kashif <ahmed.kashif1001@gmail.com>
2023-12-09 23:58:17 +0500

    promises-problem1

--- JavaScript Mastery/ES6/Promises/promises.js
+++ JavaScript Mastery/ES6/Promises/promises.js
@@ -1,5 +1,8 @@
 //Promises are like real promises that we make. In Javascript promises are used to run a part of code. If the code executes then the promise is fulfilled otherwise the promise is rejected.
 
+//Promises Problem #1
 
-let complete = true;
+const ans = new Promise((resolve, reject)=> ) {
+
+}
 

e81f8941529040a2c5423475496cdf9409d2f393
Kashif <ahmed.kashif1001@gmail.com>
2023-12-14 23:46:19 +0500

    higher order functions

---
+++ JavaScript Mastery/5-Functions/advancedfunctions.js
@@ -0,0 +1,12 @@
+
+/* order functions are the functions which accept a function in a paramter or return a function or both*/
+
+//ForEach Method always takes another function inside it, so forEach is a higher order function
+
+
+
+function abcd(val){
+
+}
+
+abcd(function())  //abcd is the higher order function here

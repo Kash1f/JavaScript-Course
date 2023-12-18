2149995da527803ad10bf681f28c1879568ce092
Kashif <ahmed.kashif1001@gmail.com>
2023-12-18 19:49:35 +0500

    synchronous vs asynchronous programming

---
+++ JavaScript Mastery/9-ES6/1-Sync vs Async/setTimeout.js
@@ -0,0 +1,13 @@
+//synchronous vs asynchronous programming
+
+//synchronous programming:
+
+console.log("script started");
+
+for(let i = 1; i<10; i++){
+    console.log("inside loop");
+}
+
+console.log("script ended");
+
+//this is synchronous programming as the code executes line by line

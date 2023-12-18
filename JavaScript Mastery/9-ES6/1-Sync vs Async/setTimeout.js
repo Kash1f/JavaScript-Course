aa6896ee351ed4a706d865f7588bccc94315bf84
Kashif <ahmed.kashif1001@gmail.com>
2023-12-18 22:57:44 +0500

    setTimeout

--- JavaScript Mastery/9-ES6/1-Sync vs Async/setTimeout.js
+++ JavaScript Mastery/9-ES6/1-Sync vs Async/setTimeout.js
@@ -11,3 +11,15 @@ for(let i = 1; i<10; i++){
 console.log("script ended");
 
 //this is synchronous programming as the code executes line by line
+
+//setTimeout
+
+//as an input aik function lega aur doosra argument lega k kitni dair baad ye function call karna hai
+
+console.log("script start");
+
+function hello(){
+    console.log("Hello world");
+}
+
+setTimeout(hello, 1000)

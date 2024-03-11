12e382322b4d506264cf64c4c4b9681d063e7cfa
Kashif <ahmed.kashif1001@gmail.com>
2024-03-11 23:59:22 +0500

    xhr using promises

--- JavaScript Mastery/9-ES6/4-AJAX/errorhandling.js
+++ JavaScript Mastery/9-ES6/4-AJAX/errorhandling.js
@@ -1,8 +1,10 @@
 const URL = "https://jsonplaceholder.typicode.com/posts"
 
-//xhr object ko use kar k apni request configure karni hai
 const xhr = new XMLHttpRequest();
-// console.log(xhr);
 
-//step1: xhr object pe open method use karna hai
 xhr.open("GET",URL)
+xhr.onload = () => {
+    const data = JSON.parse(xhr.response);
+    console.log(data);
+}
+xhr.send()
---
+++ JavaScript Mastery/9-ES6/4-AJAX/xhr-promise.js
@@ -0,0 +1,7 @@
+const URL = "https://jsonplaceholder.typicode.com/posts"
+
+function sendRequest(method, url){
+    return new Promise(function(resolve,reject){
+        const xhr = new XMLHttpRequest();
+    }
+}

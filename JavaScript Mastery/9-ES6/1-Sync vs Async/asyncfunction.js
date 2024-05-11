dcc05b746a56b7bb5acca3edcdcd6fdee5e8f90f
Kashif <ahmed.kashif1001@gmail.com>
2024-05-11 12:24:22 +0500

    async await basic example

--- JavaScript Mastery/9-ES6/1-Sync vs Async/asyncfunction.js
+++ JavaScript Mastery/9-ES6/1-Sync vs Async/asyncfunction.js
@@ -1,12 +1,17 @@
+// let test = async () => "Hello";
 
-
-
-async function test(){
-    return "Hello";
-}
-
-test().then((result)=>{
-    console.log(result);
-})
+// test().then((result)=>{
+//     console.log(result);
+// })
 
 //agar ye function kuch bhi return karega aur successfully chal jata hai to ye then function apne aap call hojaiga aur jo bhi variable .then() k parameters me hoga like result in this case to jo value async function se return ho k aaigi to wo return value usme save hojaigi aur phir run hojaigi
+
+async function test() {
+  console.log("2: Message");
+  await console.log("3: Message");
+  console.log("4: Message");
+}
+
+console.log("1: Message");
+test();
+console.log("5: Message");

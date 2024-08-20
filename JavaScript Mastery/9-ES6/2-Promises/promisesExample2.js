abaa6d202615b5b34fbaabcbbb43739786da4d31
Kashif <ahmed.kashif1001@gmail.com>
2024-08-20 20:16:17 +0500

    promises produce and consume

--- JavaScript Mastery/9-ES6/2-Promises/promisesExample2.js
+++ JavaScript Mastery/9-ES6/2-Promises/promisesExample2.js
@@ -1,19 +1,23 @@
 //Promise examples
 console.log("script start");
+
 const bucket = ['coffee', 'chips', 'vegetables', 'salt','rice'];
 
+//here new keyword constructs our promise
+//pass executor functions to promise i.e resolve and reject, these are bascially parameters who itself are functions 
 const friedPromise= new Promise ((resolve,reject)=>{
     if(bucket.includes("vegetables") && bucket.includes("salt") && bucket.includes("rice")){ 
+        //here we can pass any value, object, array or string
         resolve("Fried Rice");
     }else{
         reject("Could not make it")
     }
 })
 
-//produce
+//Promise has been produced/created above
 
-//consume
-//how to consume
+//Now we have to consume this promise
+//How to consume? As a JS developer we will produce less promises and consume more
 friedPromise.then((myFriedrice)=>{
     console.log("Lets eat", myFriedrice
     
@@ -22,6 +26,8 @@ friedPromise.then((myFriedrice)=>{
 .catch((error)=>{
     console.log(error);
 })
+
+//----------------------------------------------------------------------------------------------------------------------//
 
 setTimeout(()=>{
     console.log("Hello from setTimeout");

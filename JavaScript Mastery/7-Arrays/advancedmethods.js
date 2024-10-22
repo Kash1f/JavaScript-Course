65ca810a5188663b46e7f5a7de0c0dd4c6292a6a
Kashif <ahmed.kashif1001@gmail.com>
2024-10-22 18:06:02 +0500

    advanced array methods

---
+++ JavaScript Mastery/7-Arrays/advancedmethods.js
@@ -0,0 +1,46 @@
+
+//some method
+let nums = [1,2,1,3,4,5,6];
+
+let isGreatherThan = nums.some((n) => n>3)
+    
+console.log(isGreatherThan);
+
+//find method
+//returns only the first element that satisfies the condition
+const numbers = [1, 2, 3, 4, 5];
+
+const found = numbers.find(num => num > 3);
+
+console.log(found);
+
+//Here, 5 did not get printed as the program stopped once the first value was found that satisfies the condition
+
+//filter method
+
+const numbers2 = [1, 2, 3, 4, 5, 6];
+
+// Use filter to get all even numbers
+const evenNumbers = numbers2.filter(num => num % 2 === 0);
+
+console.log(evenNumbers); // Output: [2, 4, 6]
+
+
+//reduce method
+const a = [5,1,3,2,6];
+
+const output = a.reduce(function(acc, curr){
+    acc = acc + curr
+    return acc;
+},0)
+
+console.log(output);
+
+//forEach method
+
+const numbers3 = [1, 2, 3, 4, 5, 6];
+
+numbers3.forEach(function(value){
+    console.log(value);
+
+})

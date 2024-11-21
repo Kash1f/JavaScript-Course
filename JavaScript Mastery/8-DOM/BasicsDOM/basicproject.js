86847edabddce765b0c984b4853dedda2a4e5fab
Kashif <ahmed.kashif1001@gmail.com>
2024-11-22 02:02:56 +0500

    event listeners with bulb example using flag logic

---
+++ JavaScript Mastery/8-DOM/BasicsDOM/basicproject.js
@@ -0,0 +1,21 @@
+
+
+//When the button ON is clicked, it should change the color of the bulb to grey.
+
+
+
+
+var bulb = document.querySelector("#bulb");
+var button = document.querySelector("button");
+
+var flag = 0;
+
+button.addEventListener("click", function(){
+  if(flag == 0){
+    bulb.style.backgroundColor = "yellow";
+    flag = 1
+}  else {
+    bulb.style.backgroundColor = "transparent";
+    flag = 0;
+}
+});
--- JavaScript Mastery/8-DOM/BasicsDOM/index.js
+++ JavaScript Mastery/8-DOM/BasicsDOM/index.js
@@ -9,7 +9,31 @@
 document.querySelector("h1");
 
 //2. Changing HTML
+//Method 1 if the usage is only once
 document.querySelector("h1").innerHTML = "Method 1 to change HTML";
 
+//Method 2 if the usage is more than once
 var a = document.querySelector("h2");
 a.innerHTML = "Method 2 to change HTML";
+
+
+//3. Changing CSS
+document.querySelector("h1").style.color = "red";
+
+var a = document.querySelector("h2");
+a.style.color = "blue";
+a.style.backgroundColor = "yellow";  //use camelCase in CSS property
+ 
+
+//4. Event Listeners
+
+var a = document.querySelector("h1");
+
+//attaching event listener(click) to h1 i.e var a, () will have two parameters, the first one is the event that which event will be performed(events are predefined), the second one is the target element.
+//Jese hi h1 pe click hoga to ye function execute hojaiga
+
+a.addEventListener("click", function(){
+    a.innerHTML = "Changed through Event Listener";
+    a.style.color = "blue";
+    a.style.backgroundColor = "yellow";
+});

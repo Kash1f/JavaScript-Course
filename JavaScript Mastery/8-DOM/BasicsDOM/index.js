6f92d416bca5114a746b810174861d35786d6081
Kashif <ahmed.kashif1001@gmail.com>
2024-11-21 23:02:49 +0500

    4 pillars of DOM

---
+++ JavaScript Mastery/8-DOM/BasicsDOM/index.js
@@ -0,0 +1,11 @@
+//4 of Pillars of DOM
+
+//1. Selection of an element - An element written in HTML can be selected in Javascript code
+//2. Changing HTML - An element written in HTML, so it's HTML can be changed while writing JS code.
+//3. Changing CSS - styling in CSS file can be changed while writing JS code.
+//4. Event Listeners - 
+
+
+
+var a = document.querySelector("h1");
+a.innerHTML = "Changed by JS";

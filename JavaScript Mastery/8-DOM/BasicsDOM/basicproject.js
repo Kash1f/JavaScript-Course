ac817e35d936c67cff9c6be01bdf6b41e01f090c
Kashif <ahmed.kashif1001@gmail.com>
2024-11-22 16:11:06 +0500

    bubble game project

--- JavaScript Mastery/8-DOM/BasicsDOM/basicproject.js
+++ JavaScript Mastery/8-DOM/BasicsDOM/basicproject.js
@@ -5,25 +5,25 @@
 
 
 
-var bulb = document.querySelector("#bulb");
-var button = document.querySelector("button");
+// var bulb = document.querySelector("#bulb");
+// var button = document.querySelector("button");
 
-var flag = 0;
+// var flag = 0;
 
-button.addEventListener("click", function(){
-  if(flag == 0){
-    bulb.style.backgroundColor = "yellow";
-    flag = 1
-}  else {
-    bulb.style.backgroundColor = "transparent";
-    flag = 0;
-}
-});
+// button.addEventListener("click", function(){
+//   if(flag == 0){
+//     bulb.style.backgroundColor = "yellow";
+//     flag = 1;
+// }  else {
+//     bulb.style.backgroundColor = "transparent";
+//     flag = 0;
+// }
+// });
 
 var bulb = document.querySelector("#bulb");
 var button = document.querySelector("button");
 
 button.addEventListener("click", function(){
     bulb.style.backgroundColor = 
-    bulb.style.backgroundColor === "yellow" ? "transparent" : "yellow";
+    bulb.style.backgroundColor === "transparent" ? "yellow" : "transparent";
 });

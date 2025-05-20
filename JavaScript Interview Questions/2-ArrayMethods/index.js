ab62abae2e49ca84b9a83954209e4b34a04cac70
Kashif <ahmed.kashif1001@gmail.com>
2025-05-20 22:43:37 +0500

    js interview questions - forEach, some, every methods

--- JavaScript Interview Questions/2-ArrayMethods/index.js
+++ JavaScript Interview Questions/2-ArrayMethods/index.js
@@ -15,7 +15,7 @@ const items = [
 //assume we want to get the items in this list that are less than or equal to 100$, we can use the filter method to filer out everything that's not under a hundred dollars
 
 
-//filter method takes a single callback function which has one parameter item, which is just each item inside the array, we'll return true or false statement on whether we want to include the item in the new array so we can say just return item.price <= 100 to the new array
+//filter method takes a single callback function which has one parameter item, each item inside the array, we'll return true or false statement on whether we want to include the item in the new array so we can say just return item.price <= 100 to the new array
 const filteredItems = items.filter((item) => {
     return item.price <= 100;
 })
@@ -25,7 +25,7 @@ console.log(filteredItems)
 
 
 //2. Map method
-//map method takes a single callback function which has one parameter item, which is just each item inside the array, we'll return a new value for each item in the array and a new array will be created with the new values
+//map method takes a single callback function which has one parameter item, each item inside the array, we'll return a new value for each item in the array and a new array will be created with new values
 
 const mappedItems = items.map((item)=>{
     return item.name
@@ -34,10 +34,34 @@ const mappedItems = items.map((item)=>{
 console.log(mappedItems) //
 
 //3. Find method
-// find method takes a single callback function which has one parameter item, which is just each item inside the array, we'll return the first item that matches the condition
+//find method takes a single callback function which has one parameter item, each item inside the array, this is just going to return thge very first item that it finds in the array that returns true for the statement that you pass inside of this find function.
 
 const foundItem = items.find((item)=>{
     return item.price === 400
 })
 
 console.log(foundItem)
+
+//4. ForEach method
+//forEach method does not return anything, it just iterates through the array and executes the callback function for each item in the array
+
+items.forEach((item)=>{
+    console.log(item.name)
+})
+
+//4. Some method
+//some method is a bit different as it returns a boolean value, it takes a single callback function which has one parameter item, each item inside the array, this is just going to return true if any of the items in the array returns true if the condition is satisfied.
+
+const hasExpensiveItems = items.some((item)=>{
+    return item.price > 100;
+})
+
+console.log(hasExpensiveItems)
+
+//5. Every method
+//this is similar to some method, except for that it checks for every item in the array to return true if the condition is satisfied
+const hasInexpensiveItems = items.every((item)=>{
+    return item.price <= 1000;
+})
+
+console.log(hasInexpensiveItems)

914c4e7143296acc15b446ff376d78e82b626e8e
Kashif <ahmed.kashif1001@gmail.com>
2025-06-03 20:49:51 +0500

    lexical scoping interview questions - examples

--- JavaScript Interview Questions/3-LexicalScoping/lexical.js
+++ JavaScript Interview Questions/3-LexicalScoping/lexical.js
@@ -1,15 +1,30 @@
+// function outer() {
+//   let name = "Ali"; //outer variable
 
+//   function inner() {
+//     console.log(name); //can access 'name'
+//   }
+//   inner(); //calling inner from within outer
+// }
 
+// outer();
 
-function a(){
-    var b = 10;
-    c();
-    function c(){
+function outer() {
+  let name = "Ali"; //outer variable
 
+  function innerOne() {
+    function innerMost() {
+      console.log("innerMost", name); //can access 'name'
     }
+    console.log("innerOne", name); //can access 'name'
+    innerMost(); //calling innerMost from within innerOne
+  }
+  function innerTwo() {
+    console.log("innerTwo", name); //can also access 'name'
+  }
+
+  innerOne(); //calling inner from within outer
+  innerTwo(); //calling innerTwo from within outer
 }
 
-
-a();
-console.log(b);
-
+outer();

46ac7e685fcf51c882ba299787a90e336debde46
Kashif <ahmed.kashif1001@gmail.com>
2025-08-21 17:22:46 +0500

    es6 classes and objects

--- JavaScript Interview Questions/5-ES6/functionconstructors.js
+++ JavaScript Interview Questions/5-ES6/functionconstructors.js
@@ -9,20 +9,22 @@
 // const person1 = new Person("John", "Doe", 30);
 // console.log(person1);
 
-function Person(fname, lname, age){
-    this.fname = fname;
-    this.lname = lname;
-    this.age = age;
+class Person {
+    constructor(fname, lname, age){
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+    }
 
-    this.getName = function(){
+    getName() {
         console.log(this.fname, this.lname);
     }
-    this.getAge = function(){
+    getAge() {
         console.log(this.age);
     }
 }
 
 const person1 = new Person("John", "Doe", 30);
-const person2 = new Person("Jane", "Doe", 32);
-person2.getName();
-person2.getAge();
+// const person2 = new Person("Jane", "Doe", 32);
+person1.getName();
+person1.getAge();
